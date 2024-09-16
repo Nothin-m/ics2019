ea63fa7d7ae47150dee98fb61d251721794c8338
tracer-ics2019 <tracer@njuics.org>
2024-09-16 07:01:34 -0700

    >  compile U202115586 zzd Linux ubuntu 5.15.0-121-generic #131~20.04.1-Ubuntu SMP Mon Aug 12 13:09:56 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  07:01:34 up  2:36,  1 user,  load average: 0.00, 0.02, 0.00 6d340a054584447f29bf2657f7cbcf43b9df985

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -56,7 +56,7 @@ static struct rule {
     {"0[xX][0-9a-fA-F]+", TK_HEXADECIMAL},
     {"[0-9]+", TK_DECIMAL},  //
 
-    {"\\$0|ra|[sgt]p|t[0-6]|a[0-7]|s([0-9]|1[0-1])", TK_REG},
+    {"\\$0|pc|ra|[sgt]p|t[0-6]|a[0-7]|s([0-9]|1[0-1])", TK_REG},
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
