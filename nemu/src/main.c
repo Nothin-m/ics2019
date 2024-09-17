7948a435ba687e2b2090fa45c336d0f32736c29b
tracer-ics2019 <tracer@njuics.org>
2024-09-16 19:14:04 -0700

    >  compile U202115586 zzd Linux ubuntu 5.15.0-121-generic #131~20.04.1-Ubuntu SMP Mon Aug 12 13:09:56 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:14:04 up 34 min,  1 user,  load average: 0.14, 0.07, 0.10 590536df6d36c0e9fb95e2e473866cc8a227604

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -1,5 +1,38 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+
 int init_monitor(int, char *[]);
 void ui_mainloop(int);
+
+
+uint32_t expr(char *e, bool *success);
+
+static inline void test_expr() {
+  uint32_t ans=0, result =0;
+  char buf[65536];
+  
+  FILE *fp = fopen("tools/gen-expr/input", "r");
+  while (fscanf(fp, "%d", &ans) != -1 && fscanf(fp, "%[^\n]", buf) != -1) {
+    printf("ans: %u - exp: %s\n", ans, buf);
+    bool success = true;
+    result = expr(buf, &success);
+    printf("result: %u\n", result);
+    if (!success || result != ans) {
+      printf("False! Please debug!\n");
+      break;
+    } else {
+      printf("True! Cong!\n");
+    }
+    result = 0;
+    memset(buf, 0, sizeof(buf));
+  }
+  fclose(fp);
+}
+
+
 
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
@@ -7,6 +40,8 @@ int main(int argc, char *argv[]) {
 
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
+
+  test_expr();
 
   return 0;
 }
--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -5,10 +5,55 @@
 #include <assert.h>
 #include <string.h>
 
+#define NR_TOKENS 32
 // this should be enough
 static char buf[65536];
-static inline void gen_rand_expr() {
-  buf[0] = '\0';
+int pos = 0;
+int nr_tokens = 0;
+
+uint32_t choose(uint32_t n) { return rand() % n; }
+
+void gen_num() {
+  uint32_t num = rand() % 1000;
+  char buffer[40];
+  snprintf(buffer, sizeof(buffer), "%d", num);
+  int len = strlen(buffer);
+  strcpy(&buf[pos], buffer);
+  pos += len;
+  nr_tokens++;
+}
+
+
+void gen(char c){
+  if (c == '(') buf[pos] = '(';
+  if (c == ')') buf[pos] = ')';
+  pos++;
+  nr_tokens++;
+}
+
+
+ void gen_rand_op(){
+  switch (choose(4)){
+    case 0 : buf[pos]='+';break;
+    case 1 : buf[pos]='-';break;
+    case 2 : buf[pos]='*';break;
+    case 3 : buf[pos]='/';break;
+  }
+  pos++;
+  nr_tokens++;
+}
+
+
+
+ void gen_rand_expr() {
+  // if (nr_tokens + 3 >= NR_TOKENS) return;
+  switch (choose(3)){
+    case 0:  gen_num();  break;
+    case 1:  if (nr_tokens + 3 >= NR_TOKENS) return;
+             gen('(');  gen_rand_expr();  gen(')');  break;
+    case 2:  if (nr_tokens + 3 >= NR_TOKENS) return;
+             gen_rand_expr();  gen_rand_op();  gen_rand_expr();  break;
+  }
 }
 
 static char code_buf[65536];
@@ -29,6 +74,9 @@ int main(int argc, char *argv[]) {
   }
   int i;
   for (i = 0; i < loop; i ++) {
+    pos = 0;
+    nr_tokens = 0;
+    memset(buf, 0, sizeof(buf));
     gen_rand_expr();
 
     sprintf(code_buf, code_format, buf);
