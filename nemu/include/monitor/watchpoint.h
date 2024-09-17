e5d5ab0f2771c3622c7385a572ea4bf4830efdb6
tracer-ics2019 <tracer@njuics.org>
2024-09-17 03:43:08 -0700

    >  compile U202115586 zzd Linux ubuntu 5.15.0-121-generic #131~20.04.1-Ubuntu SMP Mon Aug 12 13:09:56 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  03:43:08 up 4 min,  1 user,  load average: 1.34, 0.92, 0.39 1bb9f8e8ca907900a3ac429d2c4aab76dee041d

--- nemu/include/monitor/watchpoint.h
+++ nemu/include/monitor/watchpoint.h
@@ -8,8 +8,18 @@ typedef struct watchpoint {
   struct watchpoint *next;
 
   /* TODO: Add more members if necessary */
-
+  char* expression;
+  uint32_t value;
+  uint32_t old_value;
+  bool enable;
 
 } WP;
+
+WP* new_wp();
+void free_wp(WP*);
+bool delete_wp(int);
+bool change_wp();
+void print_wp();
+
 
 #endif
--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -21,3 +21,89 @@ void init_wp_pool() {
 /* TODO: Implement the functionality of watchpoint */
 
 
+
+WP *new_wp(char* expression) {
+  WP *res = free_;
+  if (res == NULL) assert(0);
+  free_ = free_->next;
+  res->next = head;
+  head = res;
+  res->expression = (char *)malloc(strlen(expression) * sizeof(char));
+  strcpy(res->expression, expression);
+  bool success;
+  res->value = expr(res->expression, &success);
+  res->old_value = res->value;
+  res->enable = true;
+  return res;
+}
+
+void free_wp(WP *wp) {
+  WP* ptr;
+
+  if(head == wp){
+    ptr = head;
+    head = head->next;
+    ptr->next = free_;
+    free_ = ptr;
+    wp->enable = false;
+  } else {
+    for (ptr = head; ptr != NULL && ptr->next != wp; ptr = ptr->next) {}
+
+    if (ptr == NULL) {
+      Log("not find such watchpoint\n");
+    } else {
+      ptr->next = wp->next;
+      wp->next = free_;
+      free_ = wp;
+      wp->enable = false;
+    }
+  }
+}
+
+bool delete_wp(int no){
+  WP *ptr;
+  bool found = false;
+  // printf("no%d\n", no);
+  for (ptr = head; ptr != NULL; ptr = ptr->next) {
+    if (ptr->NO == no) {
+      free_wp(ptr);
+      found = true;
+      break;
+    }
+  }
+  if(!found){
+    Log("No such an activatd watchpoint\n");
+  }
+  return found;
+}
+
+
+
+bool change_wp() {
+  bool has_changed = false;
+  WP *ptr;
+  for (ptr = head; ptr != NULL; ptr = ptr->next) {
+    bool success;
+    int val = expr(ptr->expression, &success);
+    ptr->old_value = ptr->value;
+    ptr->value = val;
+    if(ptr->value!=ptr->old_value){
+      has_changed = true;
+    }
+  }
+  return has_changed;
+}
+
+
+void print_wp(){
+  WP* ptr;
+  printf("%-10s%-32s%-16s%-16s%-8s%-8s\n", "NO", "expression", "old_value",
+         "value", "change", "enable");
+  for (ptr = head; ptr != NULL; ptr = ptr->next)  {
+    char changed = (ptr->old_value == ptr->value) ? 'N' : 'Y';
+    char enabled = (ptr->enable) ? 'Y' : 'N';
+    printf("%-10d%-32s%-16d%-16d%-8c%-8c\n", ptr->NO, ptr->expression,
+           ptr->old_value, ptr->value, changed, enabled);
+  }
+}
+
