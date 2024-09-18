fbe5d1d95c2122887b91f65f5f88ecfb0629ca6f
tracer-ics2019 <tracer@njuics.org>
2024-09-17 19:23:06 -0700

    >  compile U202115586 zzd Linux ubuntu 5.15.0-122-generic #132~20.04.1-Ubuntu SMP Fri Aug 30 15:50:07 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:23:06 up 42 min,  1 user,  load average: 0.00, 0.01, 0.03 4f3336ed22c5ba918b5edd57759b86da12f0da5

--- nemu/src/isa/riscv32/exec/control.c
+++ nemu/src/isa/riscv32/exec/control.c
@@ -27,27 +27,27 @@ make_EHelper(jalr){
 make_EHelper(branch){
   decinfo.jmp_pc = cpu.pc + id_dest->val;
   switch (decinfo.isa.instr.funct3) {
-    case 0:  // beq
+    case 0:                                                            // beq
       rtl_jrelop(RELOP_EQ, &id_src->val, &id_src2->val, decinfo.jmp_pc);
       print_asm_template2(beq);
       break;
-    case 1:  // bne
+    case 1:                                                            // bne
       rtl_jrelop(RELOP_NE, &id_src->val, &id_src2->val, decinfo.jmp_pc);
       print_asm_template2(bne);
       break;
-    case 4:  // blt
+    case 4:                                                            // blt
       rtl_jrelop(RELOP_LT, &id_src->val, &id_src2->val, decinfo.jmp_pc);
       print_asm_template2(blt);
       break;
-    case 5:  // bge
+    case 5:                                                           // bge
       rtl_jrelop(RELOP_GE, &id_src->val, &id_src2->val, decinfo.jmp_pc);
       print_asm_template2(bge);
       break;
-    case 6:  // bltu
+    case 6:                                                           // bltu
       rtl_jrelop(RELOP_LTU, &id_src->val, &id_src2->val, decinfo.jmp_pc);
       print_asm_template2(bltu);
       break;
-    case 7:  // bgeu
+    case 7:                                                           // bgeu
       rtl_jrelop(RELOP_GEU, &id_src->val, &id_src2->val, decinfo.jmp_pc);
       print_asm_template2(bgeu);
       break;
