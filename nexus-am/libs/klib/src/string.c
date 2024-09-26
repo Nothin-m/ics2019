19afa409c7cb7d9da6be6c5625d6e87504717806
tracer-ics2019 <tracer@njuics.org>
2024-09-26 05:38:38 -0700

    >  run U202115586 zzd Linux ubuntu 5.15.0-122-generic #132~20.04.1-Ubuntu SMP Fri Aug 30 15:50:07 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  05:38:38 up 11 min,  1 user,  load average: 0.28, 0.15, 0.12 19639a807fda4bcf9009cde241b7bc90ff8009e7

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -3,38 +3,75 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  return 0;
+  size_t i = 0;
+  while (s[i]) i++;
+  return i;
 }
 
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+  size_t i = 0;
+  while (src[i] != '\0') {
+    dst[i]=src[i];
+    i++;
+  }
+  dst[i] = '\0';
+  return dst;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  return NULL;
+  for (size_t i = 0; src[i] != '\0' && i < n;i++){
+    dst[i] = src[i];
+  }
+  return dst;
 }
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+  size_t i = 0;
+  while (dst[i] != '\0') i++;
+  size_t j = 0;
+  while(src[j]!='\0'){
+    dst[i + j] = src[j];
+    j++;
+  }
+  dst[i + j] = '\0';
+  return dst;
 }
 
 int strcmp(const char* s1, const char* s2) {
-  return 0;
+  size_t i = 0;
+  while (s1[i] != '\0' && s1[i] == s2[i]) i++;
+  return s1[i] - s2[i];
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-  return 0;
+  size_t i = 0;
+  while (s1[i] != '\0' && i < n && s1[i] == s2[i]) i++;
+  if (i == n) return 0;
+  return s1[i] - s2[i];
 }
 
 void* memset(void* v,int c,size_t n) {
-  return NULL;
+  char* temp = v;
+  for (int i = 0; i < n; i++) {
+    temp[i] = (char)c;
+  }
+  return v;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  return NULL;
+  char* temp = out;
+  for (int i = 0; i < n; i++) {
+    temp[i] = ((char*)in)[i];
+  }
+  return out;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
+  const char* temp1 = s1;
+  const char* temp2 = s2;
+  for (int i = 0; i < n; i++) {
+    if (temp1[i] != temp2[i]) return temp1[i] - temp2[i];
+  }
   return 0;
 }
 
