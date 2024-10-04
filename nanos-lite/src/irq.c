5eeb66a6873306f4dd5a8befeb1567ea5b50ed62
tracer-ics2019 <tracer@njuics.org>
2024-10-04 05:27:02 -0700

    >  run U202115586 zzd Linux ubuntu 5.15.0-122-generic #132~20.04.1-Ubuntu SMP Fri Aug 30 15:50:07 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  05:27:02 up  1:19,  1 user,  load average: 0.04, 0.05, 0.01 c98bb3283fc8df68f1818fcfb2efb493855dd93d

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,7 +3,7 @@
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
     case _EVENT_YIELD:
-      Log("self trap");
+      do_syscall(c);
       break;
     case _EVENT_SYSCALL:
       do_syscall(c);
