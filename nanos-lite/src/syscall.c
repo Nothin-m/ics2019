29b6ae1202f19c64b0ba145eb1882db1bd75895f
tracer-ics2019 <tracer@njuics.org>
2024-10-04 06:54:05 -0700

    >  run U202115586 zzd Linux ubuntu 5.15.0-122-generic #132~20.04.1-Ubuntu SMP Fri Aug 30 15:50:07 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  06:54:05 up  2:46,  1 user,  load average: 0.18, 0.11, 0.03 841134503f4298e6aa00581f2c2a92209c996f2f

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -7,6 +7,8 @@ _Context* do_syscall(_Context *c) {
   a[1] = c->GPR2;
   a[2] = c->GPR3;
   a[3] = c->GPR4;
+  int res = 0;
+
 
   switch (a[0]) {
     case SYS_yield:
@@ -15,8 +17,13 @@ _Context* do_syscall(_Context *c) {
     case SYS_exit:
       _halt(a[1]);
       break;
+    case SYS_write:
+      res = fs_write(a[1], (void*)a[2], a[3]);
+      break;
+    case SYS_brk:
+      break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
-
+  c->GPRx = res;
   return NULL;
 }
